0ed098f907e6e346c76ec88f6883befa1de17405
anbcodes <anbcodes@protonmail.com>
2019-12-13 23:08:15 -0500

    added basic sheet adding

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'class-methods-use-this': 'off',
+    'default-case': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
